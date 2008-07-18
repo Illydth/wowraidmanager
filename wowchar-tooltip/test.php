cfde88a007d286075d27a237fe6dda1dd422fcd8
Douglas Wagner <dwagner@wstldwagner2.rgare.net>
2008-07-18 11:00:03 -0500

    New Test Files for Armory Sync.
    Modified the test.php files under the armory pulling code to attempt to
    write / read from the cache directory.  This should do things in the
    same way as the code does and should now identify more problems with the
    cache directory.

--- wowchar-tooltip/test.php
+++ wowchar-tooltip/test.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="pt-br" xml:lang="pt-br">
 <head>
 <title>TEST WOW ARMORY FILE</title>
-<link rel="stylesheet" href="../styles/prosilver/theme/stylesheet.css" type="text/css" />
+<!-- <link rel="stylesheet" href="../styles/prosilver/theme/stylesheet.css" type="text/css" /> -->
 </head>
 <body>
 <h2>TEST WOW ARMORY FILE</h2>
@@ -11,6 +11,11 @@ ini_set('user_agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de-DE; rv:1.8.1)
 
 $url = 'http://eu.wowarmory.com/item-tooltip.xml?i=33682';
 
+//MODIFY THESE FOR YOUR TEST
+$realm='TestRealm';
+$character='TestChar';
+$lang='en';
+
 $file_get_contents = (function_exists('file_get_contents')) ? "<font color='green'>YES</font>" : "<font color='red'>NO</font>";
 $stream_context_create = (function_exists('stream_context_create')) ? "<font color='green'>YES</font>" : "<font color='red'>NO</font>";
 $arm = (file_get_contents($url)) ? "<font color='green'>YES</font>" : "<font color='red'>NO</font>";
@@ -18,6 +23,23 @@ $curl = (function_exists('curl_init')) ? "<font color='green'>YES</font>" : "<fo
 $testcache = (is_writable("cache")) ? "<font color='green'>YES</font>" : "<font color='red'>NO</font>";
 $fopen = (ini_get('allow_url_fopen') == 1) ? "<font color='green'>YES</font>" : "<font color='red'>NO</font>";
 
+//Test Write to cache directory:
+$filename = 'cache/'.$realm.'-'.$character.'-'.$lang.'-name.html';
+$link = '<a href="' . ARMORY . '/character-sheet.xml?r='.$realm.'&n='.urlencode($character).'" target="new" onmouseover="tooltip_charid(\''.str_replace("'","\'",$realm).'\',\''.$lang.'\',\''.$character.'\')" onmouseout="tooltip_close_char()">'.$character.'</a>';
+
+$handle = @fopen($filename, 'w+');
+@fwrite($handle, $link);
+fclose($handle);
+$file_exists=(file_exists($filename)) ? "<font color='green'>YES</font>" : "<font color='red'>NO</font>";
+
+//Read from cache directory to complete test:
+$handle = @fopen($filename, 'r');
+$link2 = fread($handle,1000);
+fclose($handle);
+echo $link2;
+$read_from_file=($link2 != '') ? "<font color='green'>YES</font>" : "<font color='red'>NO</font>";
+
+echo "<br /> <hr><hr>";
 //Server info
 echo "<br /> Server type: ". $_SERVER['SERVER_SOFTWARE'];
 //PHP version
@@ -32,6 +54,10 @@ echo "<br />stream_context_create is Enabled: " . $stream_context_create;
 echo "<br />fopen is Enabled: " . $fopen;
 // Test if your server can make a connection with armory
 echo "<br />Connection with armory: ". $arm;
+// Test for proper file write to cache directory:
+echo "<br />Cache Directory Write Succeeded?: ". $file_exists;
+// Test if we correctly wrote and read from the cache directory.
+echo "<br />Read from Cache Directory: ". $read_from_file;
 // Test
 echo "<br />Test cache: ". $testcache;
 echo "<br />Armory: ". ARMORY;
