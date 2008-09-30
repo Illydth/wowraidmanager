3d1c8ce9000ea207ec42eb2728dea47ae95f83f6
Douglas Wagner <dwagner@wstldwagner2.rgare.net>
2008-09-30 13:17:18 -0500

    Removing config.php from checkin.

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-global $phpraid_config;
-$phpraid_config['db_name'] = '';
-$phpraid_config['db_host'] = '';
-$phpraid_config['db_user'] = '';
-$phpraid_config['db_pass'] = '';
-$phpraid_config['db_prefix'] = '';
-?>
