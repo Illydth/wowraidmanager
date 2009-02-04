ae754574d22cd3885e9bb1165bef14cd51508536
Douglas Wagner <douglasw@wagnerweb.org>
2009-02-04 16:42:06 -0600

    Junk File

---
+++ raid_lua/phpRaid_Data.lua
@@ -0,0 +1,396 @@
+﻿phpRaid_Data = {
+	["lua_version"] = "31",
+	["file_stamp"] = "20090203212633",
+	["guild"] = "Tenebrae Cór",
+	["server"] = "Alleria",
+	["raid_count"] = "7",
+	["raids"] = {
+		[0] = {
+			["location"] = "Officer Meeting",
+			["date"] = "10/31/2008",
+			["invite_time"] = "2000",
+			["start_time"] = "2100",
+			["event_type"] = "4",
+			["description"] = "Officer Meeting, Officers Only",
+			["queue_count"] = "0",
+			["deathknight_count"] = "1",
+			["druids_count"] = "0",
+			["hunters_count"] = "0",
+			["mages_count"] = "0",
+			["paladins_count"] = "0",
+			["priests_count"] = "0",
+			["rogues_count"] = "1",
+			["shaman_count"] = "0",
+			["warlocks_count"] = "0",
+			["warriors_count"] = "0",
+			["queue"] = {
+			},
+			["druids"] = {
+			},
+			["hunters"] = {
+			},
+			["mages"] = {
+			},
+			["paladins"] = {
+			},
+			["priests"] = {
+			},
+			["rogues"] = {
+				[0] = {
+					["name"] = "Derex",
+					["level"] = "78",
+					["class"] = "Rogue",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "",
+					["timestamp"] = "10/22/2008 - 11:35 am",
+				},
+			},
+			["shaman"] = {
+			},
+			["warlocks"] = {
+			},
+			["warriors"] = {
+			},
+			["deathknights"] = {
+				[0] = {
+					["name"] = "Test",
+					["level"] = "70",
+					["class"] = "Death Knight",
+					["race"] = "Draenei",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "",
+					["timestamp"] = "10/23/2008 - 04:16 pm",
+				},
+			},
+		},
+		[1] = {
+			["location"] = "Black Temple",
+			["date"] = "10/31/2008",
+			["invite_time"] = "2030",
+			["start_time"] = "2100",
+			["event_type"] = "1",
+			["description"] = "A test description.",
+			["queue_count"] = "0",
+			["deathknight_count"] = "0",
+			["druids_count"] = "0",
+			["hunters_count"] = "0",
+			["mages_count"] = "0",
+			["paladins_count"] = "0",
+			["priests_count"] = "0",
+			["rogues_count"] = "0",
+			["shaman_count"] = "0",
+			["warlocks_count"] = "0",
+			["warriors_count"] = "0",
+			["queue"] = {
+			},
+			["druids"] = {
+			},
+			["hunters"] = {
+			},
+			["mages"] = {
+			},
+			["paladins"] = {
+			},
+			["priests"] = {
+			},
+			["rogues"] = {
+			},
+			["shaman"] = {
+			},
+			["warlocks"] = {
+			},
+			["warriors"] = {
+			},
+			["deathknights"] = {
+			},
+		},
+		[2] = {
+			["location"] = "Gruul\'s Lair",
+			["date"] = "11/03/2008",
+			["invite_time"] = "1300",
+			["start_time"] = "1200",
+			["event_type"] = "1",
+			["description"] = "This is a test Description with a line break.\n\nSee, Line break.\n\nÐé®èx`§ ®ãîd",
+			["queue_count"] = "0",
+			["deathknight_count"] = "0",
+			["druids_count"] = "0",
+			["hunters_count"] = "0",
+			["mages_count"] = "0",
+			["paladins_count"] = "0",
+			["priests_count"] = "0",
+			["rogues_count"] = "1",
+			["shaman_count"] = "0",
+			["warlocks_count"] = "0",
+			["warriors_count"] = "0",
+			["queue"] = {
+			},
+			["druids"] = {
+			},
+			["hunters"] = {
+			},
+			["mages"] = {
+			},
+			["paladins"] = {
+			},
+			["priests"] = {
+			},
+			["rogues"] = {
+				[0] = {
+					["name"] = "Derex",
+					["level"] = "78",
+					["class"] = "Rogue",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "This is a test Comment\n\nWith Line Breaks.",
+					["timestamp"] = "11/03/2008 - 10:30 am",
+				},
+			},
+			["shaman"] = {
+			},
+			["warlocks"] = {
+			},
+			["warriors"] = {
+			},
+			["deathknights"] = {
+			},
+		},
+		[3] = {
+			["location"] = "Karazhan",
+			["date"] = "11/06/2008",
+			["invite_time"] = "1600",
+			["start_time"] = "1600",
+			["event_type"] = "1",
+			["description"] = "-",
+			["queue_count"] = "0",
+			["deathknight_count"] = "0",
+			["druids_count"] = "0",
+			["hunters_count"] = "0",
+			["mages_count"] = "0",
+			["paladins_count"] = "0",
+			["priests_count"] = "0",
+			["rogues_count"] = "0",
+			["shaman_count"] = "0",
+			["warlocks_count"] = "0",
+			["warriors_count"] = "0",
+			["queue"] = {
+			},
+			["druids"] = {
+			},
+			["hunters"] = {
+			},
+			["mages"] = {
+			},
+			["paladins"] = {
+			},
+			["priests"] = {
+			},
+			["rogues"] = {
+			},
+			["shaman"] = {
+			},
+			["warlocks"] = {
+			},
+			["warriors"] = {
+			},
+			["deathknights"] = {
+			},
+		},
+		[4] = {
+			["location"] = "Karazhan",
+			["date"] = "11/17/2008",
+			["invite_time"] = "0800",
+			["start_time"] = "0900",
+			["event_type"] = "1",
+			["description"] = "This is a test of the description.  Since this does not include any quotes this should show up perfectly fine.\n\nNew Line.",
+			["queue_count"] = "1",
+			["deathknight_count"] = "1",
+			["druids_count"] = "0",
+			["hunters_count"] = "0",
+			["mages_count"] = "0",
+			["paladins_count"] = "0",
+			["priests_count"] = "0",
+			["rogues_count"] = "0",
+			["shaman_count"] = "0",
+			["warlocks_count"] = "0",
+			["warriors_count"] = "0",
+			["queue"] = {
+				[0] = {
+					["name"] = "Derex",
+					["level"] = "78",
+					["class"] = "Rogue",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "This is a long long statement.  It is now the time for all good men to take up arms and protect their freedoms.  Join us now in doing something.",
+					["timestamp"] = "08/26/2008 - 11:05 am",
+				},
+			},
+			["druids"] = {
+			},
+			["hunters"] = {
+			},
+			["mages"] = {
+			},
+			["paladins"] = {
+			},
+			["priests"] = {
+			},
+			["rogues"] = {
+			},
+			["shaman"] = {
+			},
+			["warlocks"] = {
+			},
+			["warriors"] = {
+			},
+			["deathknights"] = {
+				[0] = {
+					["name"] = "Déréx",
+					["level"] = "70",
+					["class"] = "Death Knight",
+					["race"] = "Human",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "This is a comment test.\n\nDéréx",
+					["timestamp"] = "11/11/2008 - 01:40 pm",
+				},
+			},
+		},
+		[5] = {
+			["location"] = "Karazhan",
+			["date"] = "11/18/2008",
+			["invite_time"] = "0100",
+			["start_time"] = "0100",
+			["event_type"] = "1",
+			["description"] = "-",
+			["queue_count"] = "1",
+			["deathknight_count"] = "0",
+			["druids_count"] = "0",
+			["hunters_count"] = "0",
+			["mages_count"] = "0",
+			["paladins_count"] = "0",
+			["priests_count"] = "0",
+			["rogues_count"] = "1",
+			["shaman_count"] = "0",
+			["warlocks_count"] = "0",
+			["warriors_count"] = "1",
+			["queue"] = {
+				[0] = {
+					["name"] = "Senna",
+					["level"] = "70",
+					["class"] = "Rogue",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "Moderator",
+					["comment"] = "",
+					["timestamp"] = "10/22/2008 - 11:33 am",
+				},
+			},
+			["druids"] = {
+			},
+			["hunters"] = {
+			},
+			["mages"] = {
+			},
+			["paladins"] = {
+			},
+			["priests"] = {
+			},
+			["rogues"] = {
+				[0] = {
+					["name"] = "Derex",
+					["level"] = "78",
+					["class"] = "Rogue",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "",
+					["timestamp"] = "10/22/2008 - 11:34 am",
+				},
+			},
+			["shaman"] = {
+			},
+			["warlocks"] = {
+			},
+			["warriors"] = {
+				[0] = {
+					["name"] = "Triden",
+					["level"] = "70",
+					["class"] = "Warrior",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "User",
+					["comment"] = "",
+					["timestamp"] = "10/22/2008 - 11:34 am",
+				},
+			},
+			["deathknights"] = {
+			},
+		},
+		[6] = {
+			["location"] = "Karazhan",
+			["date"] = "11/21/2008",
+			["invite_time"] = "1300",
+			["start_time"] = "1300",
+			["event_type"] = "1",
+			["description"] = "Test",
+			["queue_count"] = "2",
+			["deathknight_count"] = "0",
+			["druids_count"] = "0",
+			["hunters_count"] = "0",
+			["mages_count"] = "0",
+			["paladins_count"] = "0",
+			["priests_count"] = "0",
+			["rogues_count"] = "0",
+			["shaman_count"] = "0",
+			["warlocks_count"] = "0",
+			["warriors_count"] = "0",
+			["queue"] = {
+				[0] = {
+					["name"] = "Derex",
+					["level"] = "78",
+					["class"] = "Rogue",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "This is a comment with several \' in it.  These comment\'s should work perfectly fine with the new code in place.  Hopefully these \'s\'s will not be a problem.",
+					["timestamp"] = "08/06/2008 - 04:26 pm",
+				},
+				[1] = {
+					["name"] = "Emara",
+					["level"] = "70",
+					["class"] = "Druid",
+					["race"] = "Night Elf",
+					["team_name"] = "",
+					["access_type"] = "Admin",
+					["comment"] = "",
+					["timestamp"] = "08/29/2008 - 10:31 am",
+				},
+			},
+			["druids"] = {
+			},
+			["hunters"] = {
+			},
+			["mages"] = {
+			},
+			["paladins"] = {
+			},
+			["priests"] = {
+			},
+			["rogues"] = {
+			},
+			["shaman"] = {
+			},
+			["warlocks"] = {
+			},
+			["warriors"] = {
+			},
+			["deathknights"] = {
+			},
+		},
+	}
+}
