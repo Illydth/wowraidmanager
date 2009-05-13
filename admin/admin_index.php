d0d318c9a78663ac09fc9917567aeded34ab5cbf
Douglas Wagner <douglasw@wagnerweb.org>
2009-05-13 12:15:10 -0500

    Created main page and added "version check" information from main configuration section to it.

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -42,7 +42,92 @@ require_once("../includes/authentication.php");
 /* 
  * Data for Index Page
  */
-$wrmadminsmarty->assign('hello_var', "Hello World");
+// Version Check
+// primarily stripped from phpBB version checking
+$current_version_array = explode('.', $version);
+$major_version = (int) $current_version_array[0];
+$minor_version = (int) $current_version_array[1];
+$patch_version = (int) $current_version_array[2];
+$sub_major_version = (int) $current_version_array[3];
+$sub_minor_version = (int) $current_version_array[4];
+$sub_patch_version = (int) $current_version_array[5];
+$current_version = (int) $current_version_array[0] . '.' . (int) $current_version_array[1] . '.' . (int) $current_version_array[2] . ':' . (int) $current_version_array[3] . '.' . (int) $current_version_array[4] . '.' . (int) $current_version_array[5];
+
+$errno = 0;
+$errstr = $version_info = '';
+
+if ($fsock = @fsockopen('www.wowraidmanager.net', 80, $errno, $errstr, 10))
+{
+	@fputs($fsock, "GET /vercheck/ver_check_40.txt HTTP/1.1\r\n");
+	@fputs($fsock, "HOST: www.wowraidmanager.net\r\n");
+	@fputs($fsock, "Connection: close\r\n\r\n");
+
+	$get_info = false;
+	while (!@feof($fsock))
+	{
+		if ($get_info)
+		{
+			$version_info .= @fread($fsock, 1024);
+		}
+		else
+		{
+			if (@fgets($fsock, 1024) == "\r\n")
+			{
+				$get_info = true;
+			}
+		}
+	}
+	@fclose($fsock);
+	$version_info = explode("\n", $version_info);
+	$latest_major_version = (int) $version_info[0];
+	$latest_minor_version = (int) $version_info[1];
+	$latest_patch_revision = (int) $version_info[2];
+	$latest_sub_major_version = (int) $version_info[4];
+	$latest_sub_minor_version = (int) $version_info[5];
+	$latest_sub_patch_version = (int) $version_info[6];
+	$latest_version = (int) $version_info[0] . '.' . (int) $version_info[1] . '.' . (int) $version_info[2] . ':' . (int) $version_info[4] . '.' . (int) $version_info[5] . '.' . (int) $version_info[6];
+
+	if ($current_version == $latest_version)
+	{
+		$version_info = '<p style="color:green">' . $phprlang['configuration_version_current'] . '</p>';
+	}
+	else
+	{
+		$version_info = '<br><div class="errorHeader">' . $phprlang['configuration_version_outdated_header'] . '</div>';
+		$version_info .= '<div class="errorBody">' . sprintf($phprlang['configuration_version_outdated_message'], $latest_version, $current_version) . '</div><br>';
+	}
+}
+else
+{
+	if ($errstr)
+	{
+		$version_info = '<p style="color:red">' . sprintf($phprlang['connect_socket_error'], $errstr) . '</p>';
+	}
+	else
+	{
+		$version_info = '<p style="color:red">' . $phprlang['socket_functions_disabled'] . '</p>';
+	}
+}
+
+$wrmadminsmarty->assign('version_data',
+	array(
+		'version_info_header'=>$phprlang['configuration_version_info_header'],
+		'version_info' => $version_info,
+	)
+);
+	
+
+// WRM Version
+// MySQL Version
+// PHP Version
+// Number of Users
+// Active Users in the Last 5 Minutes
+// Most Recent Log Entries
+
+// Actions
+// Purge Board Cache
+// Purge Armory Cache
+
 
 //
 // Start output of the page.
