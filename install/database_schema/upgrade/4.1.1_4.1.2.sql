9205f0d960832979a714a558f28a638f00a54249
Illydth <douglasw@wagnerweb.org>
2011-01-11 23:00:48 -0600

    Update to add Skywall instances and icons and Worgen/Goblin Icons.
    Signed-off-by: Illydth <douglasw@wagnerweb.org>

--- install/database_schema/upgrade/4.1.1_4.1.2.sql
+++ install/database_schema/upgrade/4.1.1_4.1.2.sql
@@ -1,4 +1,12 @@
 INSERT INTO `wrm_config` VALUES ('wrm_utf8_support', '1');
 INSERT INTO `wrm_config` VALUES ('template_body_width', 'width_normal');
 
+INSERT INTO `wrm_events` ( `zone_desc`, `max`, `exp_id`, `event_type_id`, `wow_name`, `icon_path`) VALUES
+('Skywall', 5, 4, 2, 'Skywall', 'images/instances/Cataclysm_Icons/dungeons/5-Skywall.jpg'),
+('Skywall - Heroic', 5, 4, 2, 'Skywall (Heroic)', 'images/instances/Cataclysm_Icons/dungeons/5-Skywall-Heroic.jpg'),
+('Skywall - 10 Man', 10, 4, 1, 'Skywall (10)', 'images/instances/Cataclysm_Icons/raids/10-Skywall.jpg'),
+('Skywall - 25 Man', 10, 4, 1, 'Skywall (25)', 'images/instances/Cataclysm_Icons/raids/25-Skywall.jpg');
+
+UPDATE `wrm_events` SET wow_name = 'Stonecore (Heroic)' WHERE zone_desc = 'Stonecore - Heroic' LIMIT 1;
+
 INSERT INTO `wrm_version` VALUES ('4.1.2','Version 4.1.2 of WoW Raid Manager');
